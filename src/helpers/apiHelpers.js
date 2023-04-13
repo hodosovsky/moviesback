da4acef63081bbfc52d8b02c1d65eed358601340
hodosovsky <107750587+hodosovsky@users.noreply.github.com>
2023-04-13 10:34:47 +0300

    add error handler for movies

--- src/helpers/apiHelpers.js
+++ src/helpers/apiHelpers.js
@@ -11,12 +11,15 @@ const asyncWrapper = (controller) => {
 
 const errorHandler = (err, req, res, next) => {
   if (err instanceof CustomError) {
-    return res.status(err.response.status).json({ message: err.message });
+    return res.status(err.status).json({ message: err.message });
   }
-  res
-    .status(err.response.status)
-    .json({ message: err.response.data.status_message });
-  console.log(err);
+  if (err.response.status) {
+    return res
+      .status(err.response.status)
+      .json({ message: err.response.data.status_message });
+  }
+
+  res.status(500).json({ message: err.message });
 };
 
 const createToken = async (user) => {
