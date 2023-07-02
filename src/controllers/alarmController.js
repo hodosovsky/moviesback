7039b7f7bf4aa7ceb9b8e0591660f317f4f067a5
hodosovsky <107750587+hodosovsky@users.noreply.github.com>
2023-07-02 13:18:21 +0300

    add process.env

--- src/controllers/alarmController.js
+++ src/controllers/alarmController.js
@@ -1,15 +1,14 @@
+require("dotenv").config();
 const axios = require("axios");
 
-axios.defaults.headers.common['Authorization'] = '5bfa85d8:a212478ea08355118d100959b187275f'
+axios.defaults.headers.common["Authorization"] = process.env.ALARM_KEY;
 axios.defaults.baseURL = "https://api.ukrainealarm.com/api/v3/alerts";
 
-
 const getAlarmController = async (req, res, next) => {
-    console.log(axios.defaults.headers.common)
+  console.log(axios.defaults.headers.common);
   const alarms = await axios.get();
-  
- 
+
   res.json(alarms.data);
 };
 
-module.exports = {getAlarmController}
+module.exports = { getAlarmController };
